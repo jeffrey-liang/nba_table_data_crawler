19781222e6a7814674abaf795302d8c3abe7685e
jeffrey-liang <jeffreyliang317@gmail.com>
2017-01-03 20:51:07 -0800

    del extra space

--- tests.py
+++ tests.py
@@ -9,7 +9,6 @@ links = {
     '2016_17_teams_shooting': 'http://stats.nba.com/teams/shooting/#!?Season=2016-17&SeasonType=Regular%20Season&PerMode=Totals&sort=Less%20Than%205%20ft.%20FGM&dir=1',
     '2016_17_teams_hustle': 'http://stats.nba.com/teams/hustle/#!?Season=2016-17&SeasonType=Regular%20Season&PerMode=Totals',
     '2012_13_teams_opp_shooting_overall_totals': 'http://stats.nba.com/teams/opponent-shooting/#!?Season=2012-13&SeasonType=Regular%20Season&PerMode=Totals',
-
     '2012-13_teams_opp_shooting_overall_pg': 'http://stats.nba.com/teams/opponent-shooting/#!?Season=2012-13&SeasonType=Regular%20Season'
 }
 
